330538c5c917e1e82b3305ce86b9e4efe13de307
Ishita Arora <124200986+ishitarora-14@users.noreply.github.com>
2023-07-07 19:40:13 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "short_name", "'" + chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
@@ -125,7 +125,7 @@ function buildAndShowHomeHTML (categories) {
       // of how to do that.
       // ....
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-
+      
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
